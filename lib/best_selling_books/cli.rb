05e9c1abb16dd35fb15591a19298691e3c0b6453
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-11-10 21:08:59 +0000

    Fix issues and add helper methods for call

--- lib/best_selling_books/cli.rb
+++ lib/best_selling_books/cli.rb
@@ -2,45 +2,60 @@
 class BestSellingBooks::CLI
 
   def call
-    puts "Welcome to Top 10 Best Selling Books"
-    puts "Would you like to see Barnes & Nobles' or Amazon's best sellers of 2017?"
-    site = gets.strip
-    if site == "Amazon" || site == "Amazon's"
-      list_best_sellers
-    end
-
-    puts "Choose a listing number for more information. Or type exit."
-    input = nil
-    until input == "exit"
-      input = gets.strip
-      if input == "1" || "2"
-        puts "What would you like to know?"
-          puts "A. Genre"
-          puts "B. Plot Summary"
-          puts "C. Author Bio"
-          puts "D. Prices & Formats"
-          puts "E. Link"
-      end
-
-      info = gets.strip
-      case info
-      when "A", "a"
-        puts "Genre"
-      when "B", "b"
-        puts "Plot Summary"
-      when "C", "c"
-        puts "Author Bio"
-      when "D", "d"
-        puts "Price n format"
-      when "E", "e"
-        puts "Link"
-      end
-    end
+    choose_best_sellers_list
+    display_options
   end
 
   def list_best_sellers
     puts "1. Milk and Honey by Rupi Kaur"
     puts "2. Hillbilly Elegy: A Memoir of a Family and Culture in Crisis by J. D. Vance"
+  end
+
+  def choose_best_sellers_list
+    puts "Would you like to see Barnes & Nobles' or Amazon's best sellers of 2017?"
+    site = gets.strip
+    if site.downcase == "amazon" || site == "amazon's"
+      list_best_sellers
+    else
+      puts "Please type the name of the best seller's list you would like to view."
+      choose_best_sellers_list
+    end
+  end
+
+  def display_options
+    puts "Please choose a listing number for more information. Or type exit."
+    input = gets.strip
+    until input == "exit"
+      if input == "1" || input == "2"
+        puts "What would you like to know?"
+        puts "A. Genre"
+        puts "B. Plot Summary"
+        puts "C. Author Bio"
+        puts "D. Prices & Formats"
+        puts "E. Link"
+      else
+        puts "Invalid input."
+        display_options
+      end
+
+      until input == "exit"
+        input = gets.strip
+        case input
+        when "A", "a"
+          puts "Genre"
+        when "B", "b"
+          puts "Plot Summary"
+        when "C", "c"
+          puts "Author Bio"
+        when "D", "d"
+          puts "Price n format"
+        when "E", "e"
+          puts "Link"
+        when "best sellers"
+          call
+        end
+      end
+    end
   end
 
 end
